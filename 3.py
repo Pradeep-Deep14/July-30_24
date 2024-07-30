fcc1648e202691453c8bd8d1bb7ecff510dcef05
Pradeep <deeppradeep1998@gmail.com>
2024-07-30 19:16:27 +0530

    first commit

---
+++ 3.py
@@ -0,0 +1,7 @@
+#A list contains some elements count the unique number of elements in the list
+
+
+List=['Apple','Apple','Banana','Gauva']
+Dict=set(List)
+count=len(Dict)
+print("The unique number of elements in the list is:",count)
